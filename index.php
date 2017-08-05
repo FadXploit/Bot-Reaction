7d1f19cb3afb40fbc893736f21c54b277b12ae57
fadxpl017 <brianelwin24@gmail.com>
2017-08-05 08:23:24 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<form method="post" action="FADXPL017_Submit.php">
+<label for="inputToken">Input Access Token</label>
+<p>
+<input value="" type="text" name="fadillah" required autofocus>
+<p>
+<button value="submit" type="submit">Submit Access Token</button>
+</form>
