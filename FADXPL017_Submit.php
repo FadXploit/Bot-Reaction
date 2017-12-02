f676b7e976ac9f41edc0fb8865687af02ab33c11
fadxpl017 <brianelwin24@gmail.com>
2017-12-02 23:39:19 +0700

    Delete FADXPL017_Submit.php

--- FADXPL017_Submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Location: FADXPL017_BotReaction.php");
-$token=$_POST['fadillah'];
-
-$file = fopen('FADXPL017_Token.txt','w');
-               fwrite($file,$token);
-               fclose($file);
-exit;
-?>
