2cb61432b289769642a2d7a141b6f99565905be0
fadxpl017 <brianelwin24@gmail.com>
2017-12-02 23:37:54 +0700

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<form method="post" action="FADXPL017_Submit.php">
-<label for="inputToken">Input Access Token</label>
-<p>
-<input value="" type="text" name="fadillah" required autofocus>
-<p>
-<button value="submit" type="submit">Submit Access Token</button>
-</form>
